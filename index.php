a19903e0223bf63bf7b468d5d95a62e7ce52d36b
kdniazi <kdniazi@gmail.com>
2017-11-10 23:00:35 +0500

    Both After Marge

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-echo "Git Testing...";
-echo "Very Good";
-echo "I am updating form github";
+    echo "Git Testing...";
+    echo "Very Good";
+    echo "I am updating form github";
+    echo "this is line 3";
+    echo "This is run time changing..";
+ech 
 ?>
